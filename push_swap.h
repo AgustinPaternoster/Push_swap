b092ee2d64a3125029297359c9679c2f8bc7224c
pater <agustinpaternoster@gmail.com>
2024-03-23 17:47:41 +0100

    insertion sort

---
+++ push_swap.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_SWAP
+#define PUSH_SWAP
+
+#include <stdio.h>
+
+void buble_sort(int *a, int s);
+void simple_sort(int *a, int s);
+void selection_sort(int *a, int s);
+void insertion_sort(int * a, int size);
+
+
+#endif
--- test_orden_1.c
+++ test_orden_1.c
@@ -1,76 +1,5 @@
-#include<stdio.h>
+#include "push_swap.h"
 
-//////////////////////////////
-
-void order_al(int *a, int s)
-{
-    int i,j,swap;
-    i = 0;
-    int size = s;
-    while(i < size)
-    {
-        j = 0;
-        while (j < size -1 - i)
-        {
-            if(a[j] > a[j+1])
-            {
-                swap = a[j];
-                a[j] = a[j+1];
-                a[j+1] = swap;
-            }    
-            j++;
-        }
-        i++;
-    }
-}
-
-void swap(int * a, int x , int y)
-{
-	int tmp;
-	tmp = a[x];
-	a[x] = a[y];
-	a[y] = tmp;
-}
-
-void order_al2(int *a, int s)
-{
-    int i;
-    int j;
-    int minidx;
-    
-	i = 0;
-    while (i < s -1)
-    {
-        j = i;
-		minidx = i;
-		while ( j < s - 1 )
-		{
-			if (a[minidx] > a[j+1])
-				swap(a,minidx,j+1);
-			j++;
-		}
-		i++;	
-    }
-    
-}
-
-void order_al3(int *a, int s)
-{
-	int i;
-	int tmp;
-
-	i = 0;
-	while (i < s - 1)
-	{
-		if (a[i] > a[i+1] )
-		{
-			swap(a,i,i+1);
-			i = 0;
-			continue;	
-		}
-		i++;
-	}
-}
 int main(void)
 {
     int a[10]={23,2,4,55,65,7,5,11,10,34}; 
@@ -79,7 +8,7 @@ int main(void)
         printf("%d ", a[i]);
     printf("\n");
     printf("----------------------\n");
-	order_al3(a,10);
+	insertion_sort(a,10);
     for(int i = 0; i < 10; i++)
         printf("%d ", a[i]);
     return (0);
